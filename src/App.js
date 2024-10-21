2ffe874251ac8baacb9b1a053f43988c699ece6a
[dy6379] <[telly6379@naver.com]>
2024-10-21 16:55:57 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./header.css";
+import { LiaBarsSolid } from "react-icons/lia";
+import { IoSearchSharp } from "react-icons/io5";
+import { AiOutlineUser } from "react-icons/ai";
+import { BsTruck } from "react-icons/bs";
+import { BsCart2 } from "react-icons/bs";
+import { TbSquares } from "react-icons/tb";
+
+import logo from "./img/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className="info-bar">
+          <div className="info-bar_category">
+            <LiaBarsSolid size="30" />
+          </div>
+          <div className="info-bar_logo">
+            <img className="logo11" alt="11번가" src={logo} />
+          </div>
+          <div className="info-bar_search">
+            <IoSearchSharp size="30" />
+          </div>
+          <div className="info-bar_icons">
+            <AiOutlineUser alt="마이" />
+            <BsTruck alt="주문/배송현황" />
+            <BsCart2 alt="장바구니" />
+            <TbSquares alt="최근 본 상품" />
+          </div>
+        </div>
+        <nav className="navigation-bar">
+          <div className="navigation-bar_navigations">
+            <div>베스트</div>
+            <div>쇼킹딜</div>
+            <div>슈팅배송</div>
+            <div>쿠폰/기획전</div>
+            <span>|</span> {/* 구분 기호 */}
+            <div>신선식품</div>
+            <div>장보기</div>
+            <div>9900원샵</div>
+            <div>리퍼블리</div>
+            <div>T공식대리점</div>
+            <span>|</span> {/* 구분 기호 */}
+            <div>아마존</div>
+          </div>
+          <div className="navigation-bar_my">마이 | 바로가기 OFF</div>
+        </nav>
       </header>
-    </div>
+      <main>메인</main>
+      <footer>하단</footer>
+    </>
   );
 }
 
---
+++ src/functions/Dropdown.js
@@ -0,0 +1,3 @@
+import React, { useState } from "react";
+
+const options = [{ label: "통합검색", value: "full_search" }];
