d4b256cddecad94bcd045558880d57c343a3d105
hhaemin <ahrgoals@naver.com>
2024-10-24 01:55:33 +0900

    오류 수정중

--- src/components/TimeDeal.jsx
+++ src/components/TimeDeal.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 
 const TimeDeal = () => {
-  const [time, setTime] = useState(7200); // 남은 시간을 초 단위로 설정(2시간)
+  const [time, setTime] = useState(3600); // 남은 시간을 초 단위로 설정(1시간)
   const [shuffledProducts, setShuffledProducts] = useState([]);
 
   // 남은 시간을 줄이는 타이머 함수
@@ -43,7 +43,9 @@ const TimeDeal = () => {
       return shuffledArray;
     };
 
-    setShuffledProducts(shuffleArray(products)); //  처음에만 섞은 상품 배열을 상태로 설정
+    // 처음에만 섞은 상품 배열을 상태로 설정,
+    // 설정 안할시, 초가 바뀔때마다 상품이 섞여서 계속 바뀌는 현상 발생
+    setShuffledProducts(shuffleArray(products));
   }, []);
 
   return (
@@ -52,7 +54,12 @@ const TimeDeal = () => {
       {/* 섹션 제목 */}
       <div className="flex justify-between">
         <h2 className="text-[23px] font-extrabold text-black">타임딜</h2>
-        <button>더보기﹥</button>
+        <div>
+          <button className="text-[15px] text-gray-600 hover:underline">
+            더보기
+          </button>
+          <span className="text-[15px] text-gray-600 ml-[2px]">﹥</span>
+        </div>
       </div>
 
       {/* Swiper로 상품 넘기는 기능 구현 */}
@@ -65,30 +72,30 @@ const TimeDeal = () => {
         {shuffledProducts.map((product) => (
           <SwiperSlide
             key={product.id}
-            className="hover:shadow-xl transition-shadow"
+            className="z-10 hover:shadow-xl transition-shadow"
           >
             {/* 상품 상세정보 박스 */}
-            <div className="flex flex-row justify-stretch bg-white mt-4">
+            <div className="flex bg-white mt-4">
               {/* 상품 이미지 */}
-              <div className="basis-1/2 aspect-h-1 aspect-w-3 overflow-hidden border border-gray-300">
+              <div className="w-1/2 aspect-h-1 aspect-w-3 overflow-hidden border border-gray-300">
                 <img
                   alt={product.mainImageAlt}
                   src={product.mainImageSrc}
-                  className="object-center object-cover h-full w-full"
+                  className="object-center object-fill h-full w-full"
                 />
               </div>
 
               {/* 상품 상세정보 */}
-              <div className="flex flex-col items-start basis-1/2 ">
+              <div className="flex flex-col justify-between w-1/2 ">
                 {/* 타이머 */}
-                <div className="">
+                <div>
                   <p className="text-[35px] text-red-500 font-extrabold">
                     {formatTime(time)}
                   </p>
                 </div>
 
                 {/* 상품 태그 */}
-                <div className="flex justify-center">
+                <div className="flex items-start mt-3">
                   <p className=" bg-sale-color border p-[2px] border-sale-btn text-[13px] text-sale-btn inline-block ">
                     ↓즉시할인중
                   </p>
@@ -97,7 +104,6 @@ const TimeDeal = () => {
                 {/* 상품명 */}
                 <div className="mt-1">
                   <h3 className="text-[17px] text-black font-semibold">
-                    <span aria-hidden="true" className="absolute inset-0" />
                     {product.name}
                   </h3>
                 </div>
@@ -134,21 +140,29 @@ const TimeDeal = () => {
                 </div>
 
                 {/* 배송 */}
-                <div className="flex justify-start items-baseline mt-1">
-                  {product.freeDeliver ? (
-                    <div className="flex items-baseline">
-                      <p className="text-[12px] text-gray-600">무료배송</p>
-                    </div>
-                  ) : (
-                    <div className="w-[0px]"></div>
-                  )}
+                <div className="flex justify-start items-baseline">
+                  <div className="flex items-baseline">
+                    <p className="text-[12px] text-gray-600">무료배송</p>
+                  </div>
 
                   {/* 포인트 적립율 */}
-                  <div className="flex">
+                  <div className="flex ml-3">
                     <p className="text-[12px] text-gray-700">11pay 최대 </p>
                     <p className="text-blue-600 text-[12px]">{product.point}</p>
                     <p className="text-[12px] text-gray-700"> 적립</p>
                   </div>
+
+                  {/* 남은상품개수 */}
+                  <div className="ml-3">
+                    <p className="text-[12px] text-gray-600 mt-2">
+                      {product.remainProduct}개 남음
+                    </p>
+                  </div>
+                </div>
+
+                {/* 11번가 추천 상품 */}
+                <div className="mt-4">
+                  <p className="text-[13px] text-gray-600">11번가 추천상품</p>
                 </div>
               </div>
             </div>
